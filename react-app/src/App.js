0d8566708674244bc9a2124ae5889a6b19d69d3a
Bantita Boonyadate <bantita_boonyadate@cmu.ac.th>
2021-08-25 16:30:46 +0700

    Commit createserver and Navbar
    Create a new react application using the create-react-app and Create a Navbar

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './features/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <h1 style={{ padding: '4rem 0', margin: '0', maxWidth: 1200 }}>
+          Hello React
+        </h1>
+      </div>
+    </div>
+  )
+};
+
+export default App;
